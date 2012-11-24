f16aa980cb0777885d2abc8c22d1c76819d3b6a0
Liv Madsen <liv.madsen@gmail.com>
2012-11-24 15:17:48 +0100

    Added welcome message to appear in main.min.js

---
+++ javascript-source/LM.Welcome.js
@@ -0,0 +1,5 @@
+// Hi there!
+// I'm guessing this minified mess wasn't what you were hoping to see here, so I just want to mention, that
+// all the source files are available at https://github.com/livmadsen/livmadsen.com
+// Have a good day :)
+var LM = {};
